2c19b7b8c0fb7036dc935e16aaa8b6ecff536bd7
Ben Mrejen <benm@veriti.ai>
2024-10-06 02:11:00 +0800

    create contract

---
+++ SimpleStorage.sol
@@ -0,0 +1,23 @@
+//  SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+    uint256 favoriteNumber;
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+    Person[] people;
+    mapping(string => uint256) nameToFavoriteNumber;
+    function setFavoriteNumber(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+    function retrieve() public view returns (uint256) {
+        return favoriteNumber;
+    }
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        // removing this line as PUSH uses too much gas and makes deploying impossible
+        // people.push(Person({name: _name, favoriteNumber: _favoriteNumber}));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
