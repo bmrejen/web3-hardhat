7a556e9ae70c0f75b513bd87f6d859c5e952bece
Ben Mrejen <benm@veriti.ai>
2024-10-07 03:22:45 +0800

    switch to hardhat ignition

--- contract_bin.ts
+++
@@ -1,4 +0,0 @@
-import fs from "node:fs";
-
-const CONTRACT_BIN = fs.readFileSync("./build/SimpleStorage_sol_SimpleStorage.bin", "utf-8");
-export { CONTRACT_BIN };
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,28 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+import process from "process";
+
+if (!process.env.GANACHE_URL || !process.env.PRIVATE_KEY || !process.env.GANACHE_PRIVATE_KEY) {
+  throw new Error("Missing environment variables");
+}
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.8"
+  solidity: "0.8.8",
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
+  networks: {
+    ganache: {
+      url: process.env.GANACHE_URL,
+      accounts: [process.env.GANACHE_PRIVATE_KEY],
+      chainId: 1337
+    },
+    sepolia: {
+      url: process.env.RPC_URL_SEPOLIA,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
 
 export default config;
