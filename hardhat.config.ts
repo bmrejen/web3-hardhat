da78eb81009e8d0b2be62c7e44d2c95bff9d266e
Ben Mrejen <benm@veriti.ai>
2024-10-08 19:46:31 +0800

    add task to get block number

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "dotenv/config";
 import process from "process";
+import "./tasks/block-number";
 
 if (!process.env.GANACHE_URL || !process.env.PRIVATE_KEY || !process.env.GANACHE_PRIVATE_KEY) {
   throw new Error("Missing environment variables");
@@ -12,7 +13,19 @@ const config: HardhatUserConfig = {
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY
   },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    gasPrice: 42, // do not forget this line or it does not work
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY // to get prices in USD
+  },
   networks: {
+    localhost: {
+      url: "http://localhost:8545",
+      chainId: 31337
+    },
     ganache: {
       url: process.env.GANACHE_URL,
       accounts: [process.env.GANACHE_PRIVATE_KEY],
